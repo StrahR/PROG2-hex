692c6d2313e819cd104ed3c624c80e2b09b1eb7c
jakobzmrzlikar <jakob.zmrzlikar@gmail.com>
2020-04-24 19:01:19 +0200

    Dodal osnutek preverjanja zmage

--- src/logika/Igra.java
+++ src/logika/Igra.java
@@ -53,4 +53,47 @@ public class Igra {
         return str.toString();
     }
 
+    public Boolean checkWin() {
+        Boolean top = false;
+        Boolean bottom = false;
+        int[][] smeri = {{1,0}, {0,1}, {-1,0}, {0,-1}, {-1,1}, {1,-1}};
+        Boolean[][] visited = new Boolean[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                visited[i][j] = false;
+            }
+        }
+
+        private static void floodFill(int i, int j) {
+            visited[i][j] = true;
+            if (Player.onTurn == Player.RED) {
+                if (j == 0) top = true;
+                if (j == size - 1) bottom = true;
+            } else {
+                if (i == 0) top = true;
+                if (i == size - 1) bottom = true;
+            }
+            if (top && bottom) return;
+            for (int[] s : smeri) {
+                int dx = s[0];
+                int dy = s[1];
+                if (board[i + dx][j + dy] == Player.onTurn  && !visited[i + dx][j + dy]) floodFill( + dx, j + dy); 
+        }
+
+        for (int i =0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (board[i][j] == Player.onTurn  && !visited[i][j]) {
+                    top = false;
+                    bottom = false;
+                    floodFill(i, j);
+                    if (top && bottom) return true;
+                }
+            }
+        }
+        
+        
+    }
+
+    
+
 }
