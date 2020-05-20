ddee90850cee015c7b7b27ab2e3e9fb770aff69b
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2020-05-20 21:00:34 +0200

    odstranil komentirano kodo

--- src/Hex.java
+++ src/Hex.java
@@ -1,6 +1,6 @@
 
 import logika.Igra;
-import splosno.Koordinati;
+
 import gui.Okno;
 import gui.Platno;
 
@@ -13,30 +13,5 @@ public class Hex {
         Igra game = new Igra();
         Platno platno = new Platno(game);
         Okno okno = new Okno("Hex", platno);
-
-        // game.odigraj(new Koordinati(0, 2));
-        // game.odigraj(new Koordinati(0, 3));
-        // game.odigraj(new Koordinati(1, 2));
-        // game.odigraj(new Koordinati(1, 3));
-        // game.odigraj(new Koordinati(2, 2));
-        // game.odigraj(new Koordinati(2, 3));
-        // game.odigraj(new Koordinati(3, 2));
-        // game.odigraj(new Koordinati(3, 3));
-        // game.odigraj(new Koordinati(4, 2));
-        // game.odigraj(new Koordinati(4, 3));
-        // game.odigraj(new Koordinati(5, 2));
-        // game.odigraj(new Koordinati(5, 3));
-        // game.odigraj(new Koordinati(6, 2));
-        // game.odigraj(new Koordinati(6, 3));
-        // game.odigraj(new Koordinati(7, 2));
-        // game.odigraj(new Koordinati(7, 3));
-        // game.odigraj(new Koordinati(8, 2));
-        // game.odigraj(new Koordinati(8, 3));
-        // game.odigraj(new Koordinati(9, 2));
-        // game.odigraj(new Koordinati(9, 3));
-        // game.odigraj(new Koordinati(10, 2));
-        // game.odigraj(new Koordinati(10, 3));
-        // System.out.println(game.checkWin(new Koordinati(10, 3)));
-        // System.out.println(game);
     }
 }
