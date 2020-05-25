b8d44c121bb39ce6352c3583ecbc0c8f52420873
Jakob <jakob.zmrzlikar@gmail.com>
2020-05-25 19:04:21 +0200

    Implementiral naivnega AI nasprotnika

---
+++ src/AI/Naive.java
@@ -0,0 +1,20 @@
+package AI;
+
+import logika.Igra;
+import splosno.Koordinati;
+
+import java.util.Random;
+import java.util.Set;
+
+public class Naive {
+    public static Koordinati play(Igra igra) {
+        Set<Koordinati> moves = Igra.possibleMoves();
+        int rand_int = new Random().nextInt(moves.size());
+        int i = 0;
+        for (Koordinati move: moves) {
+            if (i == rand_int)
+                return move;
+            i++;
+        }
+    }
+}
